d0fbdab304cdaea7cbe109c6ba6eab8bcc2b24d6
Andrew Kain <hello@andreww.io>
2022-07-29 11:51:14 +0400

    quick save

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails'
 import Forecast from './components/Forecast'
 import getFormattedWeatherData from './services/weatherService';
+import { useEffect, useState } from 'react'
+
 
 const App = () => {
+  const [query, setQuery] = useState({ q: "berlin" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data)
+      })
+    }
+
+    fetchWeather()
+  }, [query, units])
 
   const fetchWeather = async () => {
     const data = await getFormattedWeatherData({q: 'london'});
@@ -19,11 +34,15 @@ const App = () => {
       <TopButtons />
       <Inputs />
 
-      <TimeAndLocation />
-      <TemperatureAndDetails />
-
-      <Forecast title="HOURLY FORECAST" />
-      <Forecast title="DAILY FORECAST" />
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+    
+          <Forecast title="HOURLY FORECAST" />
+          <Forecast title="DAILY FORECAST" />
+        </div>
+      )}
     </div>
   );
 }
